3f852a39d9e57c7fc77a6c79a4fb2d22e4126a7e
Nael <nael.shuwaikh@gmail.com>
2022-11-16 21:19:37 +0100

    until the drag

--- sideMenu3/ContentView.swift
+++ sideMenu3/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showMenu = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            let drag = DragGesture()
+             GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    MainView(showMenu: self.$showMenu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.showMenu ? geometry.size.width / 2 : 0)
+                        .disabled(self.showMenu ? true : false)
+                    if self.showMenu {
+                        MenuView()
+                            .frame(width: geometry.size.width / 2)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+            }
         }
-        .padding()
+           // .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MainView: View {
+    @Binding var showMenu: Bool
+    var body: some View {
+        withAnimation {
+            Button(action: {
+                self.showMenu = true
+                print("Open the side menu")
+            }) {
+                Text("Show Menu")
+            }
+        }
     }
 }
